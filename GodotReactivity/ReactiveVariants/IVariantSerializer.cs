9296c953691eddf9a63850628deb5d230962adbb
Leonardo Raele <leonardoraele@gmail.com>
2025-02-05 17:32:30 -0300

    feat: Removed problematic implicit serialization
    The problematic code was left commented because the implicit conversion
    will be added back when this interface is split into interfaces for
    serialization and convertion.
    
    BREAKING CHANGE: Code relying on implicit serialization should be
    updated to use either Serialize() or Deserialize() methods explicitly.

--- GodotReactivity/ReactiveVariants/IVariantSerializer.cs
+++ GodotReactivity/ReactiveVariants/IVariantSerializer.cs
@@ -2,11 +2,11 @@ using Godot;
 
 namespace Raele.GodotReactivity;
 
-public interface IVariantSerializer<T> where T : IVariantSerializer<T>
+public interface IVariantSerializer<T> /*where T : IVariantSerializer<T>*/
 {
 	public abstract static Variant Serialize(T subject);
 	public abstract static T Deserialize(Variant variant);
 
-	public virtual static implicit operator Variant(T subject) => T.Serialize(subject);
-	public virtual static implicit operator T(Variant variant) => T.Deserialize(variant);
+	// public virtual static implicit operator Variant(T subject) => T.Serialize(subject);
+	// public virtual static implicit operator T(Variant variant) => T.Deserialize(variant);
 }
